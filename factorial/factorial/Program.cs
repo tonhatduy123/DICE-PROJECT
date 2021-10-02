6d31bdd6e89a69c118338b2dc9b94284b95315ef
tonhatduy123 <91723329+tonhatduy123@users.noreply.github.com>
2021-10-02 09:17:52 +0700

    Update Program.cs

--- factorial/factorial/Program.cs
+++ factorial/factorial/Program.cs
@@ -6,7 +6,15 @@ namespace factorial
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please, enter number: ");
+            int n= Convert.ToInt32(Console.ReadLine());
+            BigInteger f = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                f*= i;
+            }
+
+            Console.WriteLine("N!= " + f);
         }
     }
 }
